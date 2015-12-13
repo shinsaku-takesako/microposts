edb75632456d1215b5efa2351200004b29085c09
shinsaku_takesako <takesako@gaprise.com>
2015-12-13 07:12:11 +0000

    change branch

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,5 @@
 class UsersController < ApplicationController
+  before_action :logged_in_user, only: [:followings, :followers]
   
   def show
     @user = User.find(params[:id])
@@ -17,6 +18,16 @@ class UsersController < ApplicationController
     else
       render 'new'
     end
+  end
+  
+  def followings
+    @user = User.find(params[:id])
+    @users = @user.following_users
+    render 'following'
+  end
+  
+  def followers
+    
   end
   
   private
